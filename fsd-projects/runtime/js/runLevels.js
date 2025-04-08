a25ae9cdae533747172816fdc57d7f686d6ee728
NotDionteJ <jackson54600@students.apsb.org>
2025-04-08 13:44:24 +0000

    add runtime project

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -18,16 +18,58 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
-    var hitZoneSize = 25;
-    var damageFromObstacle = 10;
-    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-    sawBladeHitZone.x = 400;
-    sawBladeHitZone.y = 190;
-    game.addGameItem(sawBladeHitZone);
-    var obstacleImage = draw.bitmap("img/sawblade.png");
-    sawBladeHitZone.addChild(obstacleImage);
-    obstacleImage.x= -25;
-    obstacleImage.y= -25;
+    function createSawBlade(x, y) {
+      var hitZoneSize = 25;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x= -25;
+      obstacleImage.y= -25;
+    }
+    createSawBlade(400,190)
+    createSawBlade(1000,290)
+    createSawBlade(1500,190)
+    createSawBlade(2000,290)
+    createSawBlade(2030,290)
+    createSawBlade(2500,190)
+    createSawBlade(2550,190)
+    createSawBlade(2600,190)
+    createSawBlade(2720,290)
+    createSawBlade(3000,290)
+    createSawBlade(3500,190)
+    createSawBlade(4000,290)
+    createSawBlade(4500,190)
+    createSawBlade(5000,290)
+    
+    function createEnemy(x, y) {
+      // all code from TODO 7 and 8
+      var enemy = game.createGameItem("enemy", 25);
+      var redSquare = draw.rect(50, 50, "red");
+      redSquare.x = -25;
+      redSquare.y = -25;
+      enemy.addChild(redSquare);
+      enemy.x = x;
+      enemy.y = groundY - y;
+      game.addGameItem(enemy);
+      enemy.velocityX = -3;
+      enemy.onPlayerCollision = function() {
+        game.changeIntegrity(-10);
+      }
+      enemy.onProjectileCollision = function () {
+        game.increaseScore(100);
+        enemy.fadeOut();
+      }
+    }
+    createEnemy(400, groundY - 250);
+    createEnemy(950, groundY - 250);
+    createEnemy(1520, groundY -250);
+    function createReward(){
+
+    }
     function myFunctionName(parameter1Name, parameter2Name) {
       // your code to be reused goes here
       function average(x, y) {
@@ -35,6 +77,8 @@ var runLevels = function (window) {
         console.log("the average of " + x + " and " + y + " is " + average);
       }
     }
+
+    
 
     function startLevel() {
       // TODO 13 goes below here
